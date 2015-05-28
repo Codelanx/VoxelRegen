96327580b79ee36bc300e63c91d5ce37ae139c7f
Spencer Alderman <spencer@1rogue.com>
2015-05-28 01:34:37 -0400

    init commit

---
+++ src/main/java/com/codelanx/voxelregen/data/Statements.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2015 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.voxelregen.data;
+
+/**
+ * Class description for {@link Statements}
+ *
+ * @since 1.0.0
+ * @author 1Rogue
+ * @version 1.0.0
+ */
+public class Statements {
+
+    //TODO: all this
+    static final String ADD_REGION = "";
+    static final String ADD_BLOCKS_TO_REGION = "";
+    static final String GET_REGION_CONTENTS = "";
+    static final String REMOVE_REGION = "";
+    static final String GET_WORLD = "";
+    static final String CREATE_REGIONS_TABLE = "";
+    static final String CREATE_WORLDS_TABLE = "";
+    static final String CREATE_BLOCKS_TABLE = "";
+    static final String GET_REGION_META = "";
+    static final String GET_ALL_REGIONS = "";
+
+}
